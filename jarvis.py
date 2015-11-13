b57bc0ac3702977c924c25b79e21d573bd6f8401
Sumukh Sridhara <sumukh1@gmail.com>
2015-11-13 12:03:54 -0800

    fix indentation, resolves indentation issue #16

--- jarvis.py
+++ jarvis.py
@@ -4,61 +4,61 @@ import json
 import apis
 
 def square(n):
-	""" Square numbers  
+    """ Square numbers  
 
-	>>> square(2)
-	4 
-	>>> square(3)
-	8
-	"""
-	return n**n
+    >>> square(2)
+    4 
+    >>> square(3)
+    8
+    """
+    return n**n
 
 def dispatcher(command, arg):
     """ Does things """
-	if command == "weather":
-		print("Here's the weather forcast for "+arg)
-		print(apis.fetch_weather(arg))
-	if command == "square":
-		print("The square of " + n + " is " + square(n))
-	elif command == "go away":
-		print("It sounds like you no longer need my assistance")
-		print("Very well. Goodbye!")
-		return 
-	elif command == "bye":
-		print("Goodbye! Have a good day!")
-		return
-	# Reprompt the user. 
-	prompter()
+    if command == "weather":
+        print("Here's the weather forcast for "+arg)
+        print(apis.fetch_weather(arg))
+    if command == "square":
+        print("The square of " + arg + " is " + str(square(int(arg))))
+    elif command == "go away":
+        print("It sounds like you no longer need my assistance")
+        print("Very well. Goodbye!")
+        return 
+    elif command == "bye":
+        print("Goodbye! Have a good day!")
+        return
+    # Reprompt the user. 
+    prompter()
 
 
 def prompter():
     """ asks for things """
 
-	command = input("How may I help you?: [weather, square, go away, bye]")
-	if command == "weather":
-		city = input("Sure thing! What city?")
-		dispatcher(command, city)
-	if command == "square":
-		num = input("I love math! What number?")
-		dispatcher(command, num)
-	elif command == "stocks": # TODO
-		pass
-	else:
-		dispatcher(command, "")
+    command = input("How may I help you?: [weather, square, go away, bye]")
+    if command == "weather":
+        city = input("Sure thing! What city?")
+        dispatcher(command, city)
+    if command == "square":
+        num = input("I love math! What number?")
+        dispatcher(command, num)
+    elif command == "stocks": # TODO
+        pass
+    else:
+        dispatcher(command, "")
 
 def starter():
-	# TODO: Finish up command line interface
-	print("DEBUG: Called with ", sys.argv[1:])
+    # TODO: Finish up command line interface
+    print("DEBUG: Called with ", sys.argv[1:])
 
-	print("Summoning Jarvis")
-	print("Good evening!")
+    print("Summoning Jarvis")
+    print("Good evening!")
 
-	if (len(sys.argv) > 1):
-		command = sys.argv[2]
-		# TODO: Call dispatcher with args instead of prompting user. 
-	else:
-		prompter()
+    if (len(sys.argv) > 1):
+        command = sys.argv[2]
+        # TODO: Call dispatcher with args instead of prompting user. 
+    else:
+        prompter()
 
 
 if __name__ == "__main__":
-	starter()
+    starter()
